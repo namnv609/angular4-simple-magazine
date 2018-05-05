e8984b19a21c62e3ed86f2ca31dd0e7547cfe0b9
NamNV609 <namnv609@gmail.com>
2018-05-05 18:38:39 +0700

    Add migrations and seed sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create categories
+puts "Creating categories..."
+["Laravel", "Ruby on Rails", "Django"].each do |category|
+  Category.create name: category
+  puts "#{category} created."
+end
+
+# Create posts
+puts "Creating posts..."
+category_ids = Category.pluck :id
+(1..10).each do |_|
+  faker = Faker::Lorem
+  post_title = faker.sentence
+
+  Post.create title: post_title, description: faker.sentence(15), content: faker.paragraph(rand(100..200)),
+    category_id: category_ids.sample
+  puts "Post [#{post_title}] created."
+end
