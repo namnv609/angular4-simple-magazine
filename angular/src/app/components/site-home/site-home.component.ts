974c1f668707bfc98e26dad439928970f6d63a52
NamNV609 <namnv609@gmail.com>
2018-05-05 18:14:49 +0700

    Add site layout

---
+++ angular/src/app/components/site-home/site-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-home',
+  templateUrl: './site-home.component.html',
+  styles: []
+})
+export class SiteHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
